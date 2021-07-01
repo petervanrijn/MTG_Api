05cee0dcfed03e461a203cfb88eed1f5821fbfe9
petervanrijn <petervanrijn@hotmail.nl>
2021-07-01 15:49:20 +0200

    in user table added username

--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -16,6 +16,7 @@ class UserTableSeeder extends Seeder
     {
         DB::table('users')->insert([
             'name' => 'Peter',
+            'username' => 'Zhayrios',
             'email' => "Petervanrijn@hotmail.nl",
             'password' => bcrypt('Gaaf0345'),
         ]);
